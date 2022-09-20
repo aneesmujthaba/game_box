16295671d8b5ed8bd64b208377141952ba0e6c77
aneesmujthaba <anees@storilabs.com>
2022-09-20 16:07:15 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'GameBox',
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xff050B18),
         primarySwatch: Colors.blue,
